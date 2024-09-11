41e3b8bf2fb8f154500d7e4e00ebf07ea2e39dd5
Korbinian07 <korbibe@web.de>
2024-09-11 09:07:30 +0200

    initual commit

--- Vertex.java
+++ Vertex.java
@@ -1,7 +1,25 @@
 
-class Vertex{
+class Vertex {
+    public double x;
+    public double y;
+
+    public Vertex(double x, double y) {
+            this.x = x;
+            this.y = y;
+
+
+    }
 
     public static void main(String[] args) {
-        System-out.println(x:"Hello World")
+        Vertex v1 = new Vertex(42, 17);
+        Vertex v2 = new Vertex(4, 7.2);
+        System.out.println(v1);
+        v1.x = v1.x + v2.x;
+        System.out.println(v1);
     }
+    public String toString() {
+        return "("+x+","+y+")";
+
+    }
+
 }
