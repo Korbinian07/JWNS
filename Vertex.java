3821deb4d62d3ed9167f30b0416c696186cbf54b
Korbinian07 <korbibe@web.de>
2024-09-12 15:31:33 +0200

    12.09.24 Rechenübungen

--- Vertex.java
+++ Vertex.java
@@ -4,21 +4,36 @@ class Vertex {
     public double y;
 
     public Vertex(double x, double y) {
-            this.x = x;
-            this.y = y;
-
+        this.x = x;
+        this.y = y;
 
     }
 
     public static void main(String[] args) {
         Vertex v1 = new Vertex(42, 17);
         Vertex v2 = new Vertex(4, 7.2);
-        System.out.println(v1);
-        v1.x = v1.x + v2.x;
-        System.out.println(v1);
+        Vertex v3 = v1.skalarMult(2);
+        System.out.println(v1.length());
+        System.out.println(v2.length());
+
     }
+
+    public double length() {
+        return Math.sqrt(x * x + y * y);
+
+    }
+
+    public Vertex skalarMult(double s) {
+        return new Vertex(x * s, y * s);
+    }
+
+    public void skalarMultMod(double s){
+        x = x * s;
+        y = y * s;
+    }
+
     public String toString() {
-        return "("+x+","+y+")";
+        return "(" + x + "," + y + ")";
 
     }
 
