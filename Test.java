4b6489bbea8447ee303dae9cb134872923795a9e
Korbinian07 <korbibe@web.de>
2024-09-17 09:15:29 +0200

    Diverse Komische Sachen

--- Test.java
+++ Test.java
@@ -3,6 +3,13 @@ public class Test {
         Vertex v1 = new Vertex(42, 17);
         Vertex v2 = new Vertex(4, 7.2);
         Vertex v3 = v1.skalarMult(2);
+        Vertex v4 = v1.add(v2);
+
+        System.out.println(v1);
+        System.out.println(v2);
+        System.out.println(v3);
+        System.out.println(v4);
+
         System.out.println(v1.length());
         System.out.println(v2.length());
         System.out.println(v1.equals(v3));
--- Vertex.java
+++ Vertex.java
@@ -28,14 +28,14 @@ class Vertex {
 
     }
 
-    public Vertex add(Vertex v2) {
-            return new Vertex (x + v2.x, y + v2.y);
+    public Vertex add(Vertex that) {
+            return new Vertex (x + that.x, y + that.y);
 
     }
 
-    public void addMod(Vertex v2) {
-        x+= v2.x;
-        y+= v2.y;
+    public void addMod(Vertex that) {
+        x+= that.x;
+        y+= that.y;
     }
 
     public void setX(double x){
@@ -62,6 +62,17 @@ class Vertex {
 
         } 
         return false;
+    }
+
+    public Vertex sub(Vertex that) {
+        return new Vertex(x - that.x, y - that.y);
+    }
+
+    public double distance(Vertex that) {
+        double x_diff = that.x - this.x;
+        double y_diff = that.y - this.y;
+        return Math.sqrt(x_diff * x_diff + y_diff * y_diff);
+
     }
 }
 
